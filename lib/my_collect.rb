9a0ec31fb0547544ef162d85209531096fa5682a
Diane Nguyen <diane.nguyen@gmail.com>
2021-08-03 21:16:14 -0700

    My Collect assignment completed.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
 
-
+def my_collect(languages)
+    if block_given?
+        counter = 0
+        collection = []
+        while counter < languages.length
+            collection << yield(languages[counter])
+            counter += 1
+        end
+        collection
+    end
+end
